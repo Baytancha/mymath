103f593928087ca5fb2e4dc5abd540f14bfd0868
Temrus <tommytinkrem@gmail.com>
2024-07-01 00:37:46 +0300

    v1.0.0

--- mymath.go
+++ mymath.go
@@ -31,3 +31,25 @@ func Ceil(x float64) float64 {
 func Floor(x float64) float64 {
 	return math.Floor(x)
 }
+
+func Abs(x float64) float64 {
+	return math.Abs(x)
+}
+
+func Round(x float64) float64 {
+	return math.Round(x)
+}
+
+func Yn(x int, y float64) float64 {
+	return math.Yn(x, y)
+}
+
+func Acos(x float64) float64 {
+	return math.Acos(x)
+}
+func Acosh(x float64) float64 {
+	return math.Acosh(x)
+}
+func Asin(x float64) float64 {
+	return math.Asin(x)
+}
